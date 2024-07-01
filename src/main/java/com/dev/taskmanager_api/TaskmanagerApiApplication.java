7a3a5b8a8b5ae0904ebcf4cc575ccdadbf035398
Breno Bessa Borba Alcantara <a40541@alunos.ipb.pt>
2024-07-01 20:07:00 +0100

    initial commit

---
+++ src/main/java/com/dev/taskmanager_api/TaskmanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.taskmanager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/taskmanager_api/TaskmanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.taskmanager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
